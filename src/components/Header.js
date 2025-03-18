8c7ed9ba64e5250b83f5867f0d57ca98783623a2
Your Name <bouaginamaram6@gmail.com>
2025-03-18 09:04:35 +0100

    second commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import { useNavigate,Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
+  const navigate = useNavigate();
   return (
     <header className="erp-header">
       <div className="erp-header__container">
-        <h1 className="erp-header__logo">ERP Manager</h1>
+      <h1 className="erp-header__logo">
+          <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+            ERP Manager
+          </Link>
+        </h1>
         <nav className="erp-header__nav">
-          <button className="erp-header__nav-item">Admins</button>
-          <button className="erp-header__nav-item">Fournisseurs</button>
-          <button className="erp-header__nav-item">Catégories</button>
+          <button className="erp-header__nav-item" onClick={()=>navigate("/gerer-admin")}>Admins</button>
+          <button className="erp-header__nav-item" onClick={()=>navigate("/fournisseurs")}>Fournisseurs</button>
+          <button className="erp-header__nav-item" onClick={()=>navigate("/categories")}>Catégories</button>
         </nav>
       </div>
     </header>
